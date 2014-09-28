5799982bd423ddcfaa27b2d291d936fc3225a112
Ridge <ridge.liu@toursforfun.com>
2014-09-28 20:10:01 +0800

    absolute length units. in, cm, mm, pt, pc

--- media.match.js
+++ media.match.js
@@ -4,7 +4,8 @@ window.matchMedia || (window.matchMedia = function (win) {
     'use strict';
 
     // Internal globals
-    var _doc        = win.document,
+    var _dpi,
+        _doc        = win.document,
         _viewport   = _doc.documentElement,
         _queries    = [],
         _queryID    = 0,
@@ -16,6 +17,13 @@ window.matchMedia || (window.matchMedia = function (win) {
                     // not screen and
                     // screen
                     // screen and
+        _units   = {
+            cm: 2.54,
+            mm: 25.4,
+            pt: 72,
+            pc: 6,
+            'in': 1
+        },
         _typeExpr   = /\s*(only|not)?\s*(screen|print|[a-z\-]+)\s*(and)?\s*/i,
                     // (-vendor-min-width: 300px)
                     // (min-width: 300px)
@@ -31,7 +39,7 @@ window.matchMedia || (window.matchMedia = function (win) {
         /*
             _matches
          */
-        _matches = function (media) {
+        _matches = function(media) {
             // styleMedia.matchMedium IE 9 way
             if (_styleMedia && _doc.documentMode) {
                 return _styleMedia.matchMedium(media);
@@ -121,6 +129,9 @@ window.matchMedia || (window.matchMedia = function (win) {
                             if (unit === 'px') {
                                 // If unit is px
                                 value = Number(length);
+                            } else if (_units[unit]) {
+                                // If unit is absolute length units
+                                value = length * _dpi / _units[unit];
                             } else if (unit === 'em' || unit === 'rem') {
                                 // Convert relative length unit to pixels
                                 // Assumed base font size is 16px
@@ -211,7 +222,7 @@ window.matchMedia || (window.matchMedia = function (win) {
         /*
             _init
          */
-        _init = function () {
+        _init = function() {
             var head        = _doc.getElementsByTagName('head')[0],
                 style       = _doc.createElement('style'),
                 info        = null,
@@ -231,13 +242,16 @@ window.matchMedia || (window.matchMedia = function (win) {
                 dw          = screen.width,
                 dh          = screen.height,
                 c           = screen.colorDepth,
-                x           = win.devicePixelRatio;
+                logicalXDPI = screen.logicalXDPI,
+                ratio       = win.devicePixelRatio || (screen.deviceXDPI / logicalXDPI) || 1;
+
+            _dpi         = logicalXDPI || (ratio * 96);
 
             // Sets properties of '_features' that change on resize and/or orientation.
             _features['aspect-ratio'] = function() {
                 return (w() / h()).toFixed(2);
             };
-            _features['orientation']  = function() {
+            _features['orientation'] = function() {
                 return (h() >= w() ? 'portrait' : 'landscape');
             };
 
@@ -248,8 +262,8 @@ window.matchMedia || (window.matchMedia = function (win) {
             _features['device-aspect-ratio']    = (dw / dh).toFixed(2);
             _features['color']                  = c;
             _features['color-index']            = Math.pow(2, c);
-            _features['resolution']             = (x && x * 96) || screen.deviceXDPI || 96;
-            _features['device-pixel-ratio']     = x || 1;
+            _features['resolution']             = _dpi;
+            _features['device-pixel-ratio']     = ratio;
 
             style.type  = 'text/css';
             style.id    = 'mediamatchjs';
@@ -260,7 +274,7 @@ window.matchMedia || (window.matchMedia = function (win) {
             info = (win.getComputedStyle && win.getComputedStyle(style)) || style.currentStyle;
 
             // Create media blocks to test for media type
-            for ( ; typeIndex < typeLength; typeIndex++) {
+            for (; typeIndex < typeLength; typeIndex++) {
                 cssText += '@media ' + typeList[typeIndex] + ' { #mediamatchjs { z-index: ' + typeIndex + ' } }';
             }
 
